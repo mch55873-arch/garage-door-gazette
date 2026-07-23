17b19fb9103c3e72ed6c2975ebe3dbfd3166c4b9
mch55873-arch <mch55873@gmail.com>
2026-07-23 16:40:50 +0500

    feat: add optimized garage image library

--- data/site.ts
+++ data/site.ts
@@ -10,5 +10,15 @@ export const siteConfig = {
     : "/contact/",
 };
 
+export const siteImages = {
+  hero: "https://images.pexels.com/photos/28384143/pexels-photo-28384143.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  service: "https://images.pexels.com/photos/34711989/pexels-photo-34711989.jpeg?auto=compress&cs=tinysrgb&w=1400",
+  about: "https://images.pexels.com/photos/33910247/pexels-photo-33910247.jpeg?auto=compress&cs=tinysrgb&w=1400",
+  areas: "https://images.pexels.com/photos/34859642/pexels-photo-34859642.jpeg?auto=compress&cs=tinysrgb&w=1400",
+  installation: "https://images.pexels.com/photos/17072601/pexels-photo-17072601.jpeg?auto=compress&cs=tinysrgb&w=1400",
+  woodDoor: "https://images.pexels.com/photos/38264449/pexels-photo-38264449.jpeg?auto=compress&cs=tinysrgb&w=1400",
+  commercial: "https://images.pexels.com/photos/31944676/pexels-photo-31944676.jpeg?auto=compress&cs=tinysrgb&w=1400",
+} as const;
+
 export const absoluteUrl = (path = "/") =>
   `${siteConfig.url}${path.startsWith("/") ? path : `/${path}`}`;
